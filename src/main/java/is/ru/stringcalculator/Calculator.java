27eba5fb1a29093639a483a8e98ffc320d0d2312
magnusk12 <magnusk12@ru.is>
2015-10-13 20:48:58 +0000

    Support for two numbers and refactor code to toInt

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -4,7 +4,17 @@ public class Calculator {
 	public static int add(String text) {
 		if (text.equals(""))
 			return 0;
+		else if (text.contains(",")) {
+			String[] split = text.split(",");
+			return (toInt(split[0]) + toInt(split[1]) );
+		}
 		else
-			return (Integer.parseInt(text));
+			return (toInt(text));
 	}
+
+	private static int toInt(String text) {
+		return (Integer.parseInt(text));
+
+	}
+
 }
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -20,5 +20,10 @@ public class CalculatorTest {
 		assertEquals(1, Calculator.add("1"));
 	}
 
+	@Test
+	public void testTwoNumbers() {
+		assertEquals(3, Calculator.add("1,2"));
+	}
+
 
 }
