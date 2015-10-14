8320cadd049f8b71786a5a29b6c3b0135706fa4c
magnusk12 <magnusk12@ru.is>
2015-10-14 12:43:31 +0000

    more check on one and two mumbers with space

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -24,10 +24,46 @@ public class CalculatorTest {
 	public void testOneNumber() {
 		assertEquals(1, Calculator.add("1"));
 	}
+	@Test
+	public void testOneNumber11space() {
+		assertEquals(1, Calculator.add(" 1"));
+	}
+	@Test
+	public void testOneNumber12() {
+		assertEquals(1, Calculator.add(" 1 "));
+	}
+	@Test
+	public void testOneNumber13() {
+		assertEquals(1, Calculator.add("1 "));
+	}
 
 	@Test
 	public void testTwoNumbers() {
 		assertEquals(3, Calculator.add("1,2"));
+	}
+	@Test
+	public void testTwoNumbers11space() {
+		assertEquals(3, Calculator.add(" 1,2"));
+	}
+	@Test
+	public void testTwoNumbers12space() {
+		assertEquals(3, Calculator.add(" 1 ,2"));
+	}
+	@Test
+	public void testTwoNumbers13space() {
+		assertEquals(3, Calculator.add("1, 2"));
+	}
+	@Test
+	public void testTwoNumbers14space() {
+		assertEquals(3, Calculator.add("1, 2 "));
+	}
+	@Test
+	public void testTwoNumbers15space() {
+		assertEquals(3, Calculator.add("1 , 2"));
+	}
+	@Test
+	public void testTwoNumbers16space() {
+		assertEquals(3, Calculator.add(" 1 , 2 "));
 	}
 
 
