e99c08096f58ee2a2b7b404673d0d8ce7fdb638e
magnusk12 <magnusk12@ru.is>
2015-10-14 16:59:52 +0000

    support for different delimter

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -1,13 +1,26 @@
 package is.ru.stringcalculator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Calculator {
 	public static int add(String text) {
 		// clean text of white spaces
 		String textW = text.replaceAll("\\s+","");
+		String splitChar = "";
+
+		if  (text.startsWith("//")) {
+			Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+			m.matches();
+			splitChar = m.group(1);
+			textW = m.group(2);
+		} else {
+			splitChar = ",";
+		}
+
 		if (textW.equals(""))
 			return 0;
-		else if (textW.contains(",")) {
-			String[] split = textW.split(",");
+		else if (textW.contains(splitChar)) {
+			String[] split = textW.split(splitChar);
 			int answer = 0;
 			for (int i=0; i< split.length; i++) {
 				answer += toInt(split[i]);
@@ -24,10 +37,14 @@ public class Calculator {
 			answer = Integer.parseInt(text);
 		} catch (NumberFormatException e) {
 			// do some error handling 
-			System.out.println("Illegal input, exiting");
+			System.out.println("Illegal input, exiting " + text);
 			System.exit(0);
 		}
 		return answer;
+	}
+
+	public static void main (String[] args) {
+		add(args[0]);
 	}
 
 }
